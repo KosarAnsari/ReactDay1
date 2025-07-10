991581272fa2057ba96d358833f1553a73dd47d0
KosarAnsari <kosaransari55@gmail.com>
2025-07-11 00:10:25 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+//we write tags of html starting from small letters and we name components starting from capital letter in react or else it will give error.
+import Header from "./Header";
+
+function App(){
+  return(
+    <div>
+      <Header/>
+      <h1>hello React 19</h1>
+      <h1>first react code</h1>
+    </div>
+  )
+}
+
+export default App;
---
+++ Header.jsx
@@ -0,0 +1,9 @@
+// we can create new files inside src.
+
+function Header(){
+    return <div>
+        <h4>header file</h4>
+    </div>
+}
+
+export default Header;
